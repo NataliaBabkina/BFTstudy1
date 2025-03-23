ff7f0848f85fa248ab8072e9f0dea8f40b7fe757
NataliaBabkina <natasha.babkina@mail.ru>
2025-03-23 20:13:58 +0300

    Третье задание

---
+++ src/main/java/lesson3/Drink.java
@@ -0,0 +1,13 @@
+package lesson3;
+
+public class Drink {
+    public String name;
+    public String size;
+
+    static String brand = "Кола";
+
+    public void printBrand() {
+        System.out.println("Бренд: " + brand);
+    }
+}
+
